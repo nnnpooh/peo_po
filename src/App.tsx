36ed2d46e0810ec33b08831ce4490483b57a0b00
Nirand <56184504+nnnpooh@users.noreply.github.com>
2022-01-20 23:13:24 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import supabase from './db';
+import { useEffect, useState } from 'react';
+
+interface peoPivotProperty {
+  peo: number;
+  po_group: number[];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [peoPivot, setPeoPivot] = useState<peoPivotProperty[]>([]);
+
+  useEffect(() => {
+    async function loadData() {
+      let { data: peo_po_pivot, error } = await supabase
+        .from('peo_po_pivot')
+        .select('*');
+
+      if (peo_po_pivot) setPeoPivot(peo_po_pivot);
+    }
+    loadData();
+  }, []);
+
+  console.log(peoPivot);
+
+  return <div className='App'></div>;
 }
 
 export default App;
+
+function processData(data: peoPivotProperty[]) {
+  return data;
+}
---
+++ src/playground.ts
@@ -0,0 +1,18 @@
+const data = [
+  {
+    peo: 1,
+    po_group: [11, 3],
+  },
+  {
+    peo: 2,
+    po_group: [6, 7],
+  },
+  {
+    peo: 3,
+    po_group: [1, 2],
+  },
+];
+
+data.map((el) => {});
+
+export {};
