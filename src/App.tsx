9d7f15b64193c018cd6cd2168d2c1cde4c643ba5
Nirand <56184504+nnnpooh@users.noreply.github.com>
2022-01-21 07:50:37 +0700

    Working on data layer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import supabase from './db';
 import { useEffect, useState } from 'react';
-
-interface peoPivotProperty {
+import 'App.css';
+interface peoPivot {
   peo: number;
   po_group: number[];
+  des_en: string;
+  des_th: string;
+}
+
+interface rowsTemplate {
+  peo: number | null;
+  po: number;
+  isMap: boolean;
+}
+
+interface dataMap extends peoPivot {
+  rows: rowsTemplate[];
 }
 
 function App() {
-  const [peoPivot, setPeoPivot] = useState<peoPivotProperty[]>([]);
+  const [peoPivot, setPeoPivot] = useState<peoPivot[]>([]);
 
   useEffect(() => {
     async function loadData() {
@@ -20,13 +32,51 @@ function App() {
     loadData();
   }, []);
 
-  console.log(peoPivot);
+  const dataMap = processData(peoPivot);
 
-  return <div className='App'></div>;
+  console.log(dataMap);
+
+  return (
+    <div className='row'>
+      <div>
+        {dataMap.map((el) => (
+          <div>
+            <div>{el.peo}</div>
+            <div>{el.des_th}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
 
-function processData(data: peoPivotProperty[]) {
-  return data;
+function processData(data: peoPivot[]): dataMap[] {
+  const allPo = [...Array(11).keys()].map((el) => el + 1);
+  const rowsTemplate: rowsTemplate[] = allPo.map((po) => {
+    return {
+      peo: null,
+      po: po,
+      isMap: false,
+    };
+  });
+
+  const dataMap: dataMap[] = data.map((el) => {
+    let rows: rowsTemplate[] = JSON.parse(JSON.stringify(rowsTemplate));
+    rows = rows.map((row) => {
+      return {
+        ...row,
+        peo: el.peo,
+        isMap: el.po_group.includes(row.po) ? true : false,
+      };
+    });
+
+    return {
+      ...el,
+      rows,
+    };
+  });
+
+  return dataMap;
 }
--- src/playground.ts
+++ src/playground.ts
@@ -2,17 +2,62 @@ const data = [
   {
     peo: 1,
     po_group: [11, 3],
+    des_en: 'AAA',
+    des_th: 'BBBB',
   },
   {
     peo: 2,
     po_group: [6, 7],
+    des_en: 'AAAA',
+    des_th: 'BBBB',
   },
   {
     peo: 3,
     po_group: [1, 2],
+    des_en: 'AAAAA',
+    des_th: 'BBBBBB',
   },
 ];
 
-data.map((el) => {});
+interface rowsTemplate {
+  peo: number | null;
+  po: number;
+  isMap: boolean;
+}
+
+interface dataMap {
+  peo: number;
+  po_group: number[];
+  des_en: string;
+  des_th: string;
+  rows: rowsTemplate[];
+}
+// Create array of 1 to 11
+const allPo = [...Array(11).keys()].map((el) => el + 1);
+const rowsTemplate: rowsTemplate[] = allPo.map((po) => {
+  return {
+    peo: null,
+    po: po,
+    isMap: false,
+  };
+});
+
+const dataMap: dataMap[] = data.map((el) => {
+  let rows: rowsTemplate[] = JSON.parse(JSON.stringify(rowsTemplate));
+  rows = rows.map((row) => {
+    return {
+      ...row,
+      peo: el.peo,
+      isMap: el.po_group.includes(row.po) ? true : false,
+    };
+  });
+
+  return {
+    ...el,
+    rows,
+  };
+});
+
+console.log(dataMap);
 
 export {};
